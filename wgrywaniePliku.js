1e1f8754d70c2cade17c2681e3810428deba4f4b
Maciej <88382036+woszu23@users.noreply.github.com>
2023-02-24 11:31:09 +0100

    Delete wgrywaniePliku.js

--- wgrywaniePliku.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-describe("E2E - Wgrywanie pliku", () => {
-    it("Wgrywanie pliku do inputa w contact us", () => {
-        cy.visit("/index.php?controller=contact")
-        cy.get("#fileUpload").attachFile("../fixtures/kotek.jpg");
-        cy.get("span.filename").should("contain", "kotek.jpg")
-    })
-})
