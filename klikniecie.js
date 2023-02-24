d2f962bd7a949cb93562bd1942465fa593b2941f
Maciej <88382036+woszu23@users.noreply.github.com>
2023-02-24 11:30:40 +0100

    Delete klikniecie.js

--- klikniecie.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-describe("E2E - Akcje kliknięcia", () => {
-    it("Kliknięcie na element", () => {
-        cy.visit("/")
-        cy.contains('[title="Contact Us"]', 'Contact us').click();
-        cy.get('[title="Return to Home"]').click();
-    })
-})
